d75d5af1cc6f1f400fa5e6e5eb0d696106716630
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-09-30 17:41:23 +0200

    Make park partner text editorial

--- src/Blocks/OfferTarget/assets/dist/scripts/editor.asset.php
+++ src/Blocks/OfferTarget/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5fd5dca04ef9e30b5dc8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '86373fc91fe038b925a9');
