f141dba05b34a5bc70d446ca27d2adf61558de4e
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-09-30 16:45:34 +0200

    Re-save asset

--- src/Blocks/OfferTransportStop/assets/dist/scripts/editor.asset.php
+++ src/Blocks/OfferTransportStop/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '35f684361c68b849634c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c4c8ca79f9926cdc4542');
