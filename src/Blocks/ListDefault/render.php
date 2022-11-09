063ecf67e0700d9968252abdc64f479df6741fc0
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-11-09 15:09:41 +0100

    Temporariyl remove filter

--- src/Blocks/ListDefault/render.php
+++ src/Blocks/ListDefault/render.php
@@ -20,7 +20,8 @@ $block_controller = new BlockController();
 $block_controller->extend($block);
 
 $classNameBase = $block['shp']['classNameBase'] ?? '';
-$show_filter = (bool) get_field('adb_show_filter');
+$show_filter = false; // Temporary hard-coding
+//$show_filter = (bool) get_field('adb_show_filter');
 
 $offer_model = new OfferModel();
 
@@ -78,13 +79,13 @@ wp_localize_script($classNameBase, 'shp_gantrisch_adb_block_list_default', [
 
 $api = shp_gantrisch_adb_get_instance()->Controller->API->getApi();
 
-//if ($show_filter) {
-wp_enqueue_script("{$classNameBase}_i18n", "https://angebote.paerke.ch/api/lib/api-17/{$api->lang_id}.js", ['jquery'], null, true);
-// wp_enqueue_script("{$classNameBase}_jquery", "https://angebote.paerke.ch/api/lib/api-17/jquery.min.js", [], null, true);
-// wp_enqueue_script("{$classNameBase}_jquery-ui", "https://angebote.paerke.ch/api/lib/api-17/jquery-ui.min.js", [], null, true);
-wp_enqueue_script("{$classNameBase}_parkapp", "https://angebote.paerke.ch/api/lib/api-17/ParkApp.min.js", ['jquery'], null, true);
-wp_enqueue_style("{$classNameBase}_parkapp-api", "https://angebote.paerke.ch/api/lib/api-17/api.css", [], null);
-//}
+if ($show_filter) {
+	wp_enqueue_script("{$classNameBase}_i18n", "https://angebote.paerke.ch/api/lib/api-17/{$api->lang_id}.js", ['jquery'], null, true);
+	wp_enqueue_script("{$classNameBase}_jquery", "https://angebote.paerke.ch/api/lib/api-17/jquery.min.js", [], null, false);
+	wp_enqueue_script("{$classNameBase}_jquery-ui", "https://angebote.paerke.ch/api/lib/api-17/jquery-ui.min.js", [], null, false);
+	wp_enqueue_script("{$classNameBase}_parkapp", "https://angebote.paerke.ch/api/lib/api-17/ParkApp.min.js", ['jquery'], null, true);
+	wp_enqueue_style("{$classNameBase}_parkapp-api", "https://angebote.paerke.ch/api/lib/api-17/api.css", [], null);
+}
 
 $count = 1;
 
@@ -96,77 +97,70 @@ $categories_info = is_array($category_ids) ? implode(', ', $category_ids) : 'all
 	<?php if ((bool) get_field('adb_show_filter')) { ?>
 		<div class="<?php echo $classNameBase; ?>__filter">
 			<?php
-			$api->show_offers_filter($category_ids, $filters);
+			//$api->show_offers_filter($category_ids, $filters);
 			?>
 		</div>
 	<?php }
 
-	$api->show_offers_list($category_ids, $filters);
-	$api->show_offers_pagination();
-
-
-	if (false) {
-
+	//$api->show_offers_list($category_ids, $filters);
+	//$api->show_offers_pagination();
 	?>
 
+	<ul class="<?php echo $classNameBase; ?>__entries">
+		<?php
+		foreach ($offers as $offer) {
 
-		<ul class="<?php echo $classNameBase; ?>__entries">
-			<?php
-			foreach ($offers as $offer) {
+			$offer = (array) $offer;
 
-				$offer = (array) $offer;
+			$button_text = esc_html($block['data']['button_text'] ?? '');
 
-				$button_text = esc_html($block['data']['button_text'] ?? '');
+			$images = $offer_model->getImages($offer['offer_id']);
+			$selected_size = $block['data']['image_size'] ?? 'small';
 
-				$images = $offer_model->getImages($offer['offer_id']);
-				$selected_size = $block['data']['image_size'] ?? 'small';
+			if (!empty($images) && isset($images[0]->{$selected_size}) && filter_var($images[0]->{$selected_size}, FILTER_VALIDATE_URL) !== false) {
+				$image_html = sprintf(
+					'<figure class="%1$s__entry-figure"><img class="%1$s__entry-image" src="%2$s" alt="%3$s" loading="%4$s"></figure>',
+					$classNameBase,
+					$images[0]->{$selected_size},
+					esc_html($offer['title']),
+					$count > (int) ($block['data']['initial_count'] ?? false) ? 'lazy' : 'eager'
+				);
+			} else {
+				$image_html = sprintf(
+					'<div class="%1$s__entry-figure %1$s__entry-figure--empty"></div>',
+					$classNameBase
+				);
+			}
+		?>
+			<li class="<?php echo $classNameBase; ?>__entry <?php echo $classNameBase; ?>__entry--<?php echo $offer['offer_id']; ?> is--hidden">
 
-				if (!empty($images) && isset($images[0]->{$selected_size}) && filter_var($images[0]->{$selected_size}, FILTER_VALIDATE_URL) !== false) {
-					$image_html = sprintf(
-						'<figure class="%1$s__entry-figure"><img class="%1$s__entry-image" src="%2$s" alt="%3$s" loading="%4$s"></figure>',
-						$classNameBase,
-						$images[0]->{$selected_size},
-						esc_html($offer['title']),
-						$count > (int) ($block['data']['initial_count'] ?? false) ? 'lazy' : 'eager'
-					);
-				} else {
-					$image_html = sprintf(
-						'<div class="%1$s__entry-figure %1$s__entry-figure--empty"></div>',
-						$classNameBase
-					);
-				}
-			?>
-				<li class="<?php echo $classNameBase; ?>__entry <?php echo $classNameBase; ?>__entry--<?php echo $offer['offer_id']; ?> is--hidden">
-
-					<div class="<?php echo $classNameBase; ?>__entry-header">
-						<div class="<?php echo $classNameBase; ?>__entry-title">
-							<a href="<?php echo $offer_controller->singleUrl($offer); ?>"><?php echo esc_html($offer['title']); ?></a>
-						</div>
-
-						<?php if (!empty($offer['location_details'])) { ?>
-							<div class="<?php echo $classNameBase; ?>__entry-location">
-								<p><?php echo esc_html($offer['location_details']); ?></p>
-							</div>
-						<?php
-						}
-						?>
+				<div class="<?php echo $classNameBase; ?>__entry-header">
+					<div class="<?php echo $classNameBase; ?>__entry-title">
+						<a href="<?php echo $offer_controller->singleUrl($offer); ?>"><?php echo esc_html($offer['title']); ?></a>
 					</div>
 
-					<?php echo $image_html; ?>
-
-					<?php if (!empty($button_text)) { ?>
-						<div class="<?php echo $classNameBase; ?>__entry-buttonwrapper">
-							<a class="<?php echo $classNameBase; ?>__entry-button" href="<?php echo $offer_controller->singleUrl($offer); ?>"><?php echo $button_text; ?></a>
+					<?php if (!empty($offer['location_details'])) { ?>
+						<div class="<?php echo $classNameBase; ?>__entry-location">
+							<p><?php echo esc_html($offer['location_details']); ?></p>
 						</div>
-					<?php } ?>
+					<?php
+					}
+					?>
+				</div>
 
-					<a class="<?php echo $classNameBase; ?>__entry-floodlink" href="<?php echo $offer_controller->singleUrl($offer); ?>"><?php echo esc_html($offer['title']); ?></a>
-				</li>
-			<?php
-				$count++;
-			}
-			?>
-		</ul>
+				<?php echo $image_html; ?>
 
-	<?php } ?>
+				<?php if (!empty($button_text)) { ?>
+					<div class="<?php echo $classNameBase; ?>__entry-buttonwrapper">
+						<a class="<?php echo $classNameBase; ?>__entry-button" href="<?php echo $offer_controller->singleUrl($offer); ?>"><?php echo $button_text; ?></a>
+					</div>
+				<?php } ?>
+
+				<a class="<?php echo $classNameBase; ?>__entry-floodlink" href="<?php echo $offer_controller->singleUrl($offer); ?>"><?php echo esc_html($offer['title']); ?></a>
+			</li>
+		<?php
+			$count++;
+		}
+		?>
+	</ul>
 </div>
