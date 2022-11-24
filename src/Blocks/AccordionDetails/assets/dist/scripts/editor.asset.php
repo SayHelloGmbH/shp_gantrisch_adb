9902a74acf5f937d24f0855f707606948c2efa75
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-11-24 12:33:46 +0100

    Disallow multiple details accordions

--- src/Blocks/AccordionDetails/assets/dist/scripts/editor.asset.php
+++ src/Blocks/AccordionDetails/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '351250f8c4dbf18f4562');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd9dcb9c9f0e674733ed5');
