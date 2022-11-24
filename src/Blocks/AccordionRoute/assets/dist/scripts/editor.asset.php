1f465d5e010a5dcb720604e5e7814171cbb4ca69
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-11-24 17:32:33 +0100

    Accordion title styling

--- src/Blocks/AccordionRoute/assets/dist/scripts/editor.asset.php
+++ src/Blocks/AccordionRoute/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a9bcec86e6c5811f782e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '033fea44719412fbb8f8');
