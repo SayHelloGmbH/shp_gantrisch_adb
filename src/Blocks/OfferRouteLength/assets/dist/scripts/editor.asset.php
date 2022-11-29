951a85a667c04ed1f3b1b6c35c9885aa30c05794
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-11-29 18:13:43 +0100

    Disallow widths

--- src/Blocks/OfferRouteLength/assets/dist/scripts/editor.asset.php
+++ src/Blocks/OfferRouteLength/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd15c80eef48a3e8e651c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b6647f1bb12c522986b6');
--- src/Blocks/OfferTime/assets/dist/scripts/editor.asset.php
+++ src/Blocks/OfferTime/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b8dd17140892925cd986');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4d23fc7b84318c9c4ea0');
