1cb80f32ed86476b3cf5abdb7d62f3fddb7e0dd7
Mark Howells-Mead <permanent.tourist@gmail.com>
2022-11-30 12:35:29 +0100

    Improve editor views

--- src/Blocks/OfferSubscription/assets/dist/scripts/editor.asset.php
+++ src/Blocks/OfferSubscription/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6ece7ea2fdd36622af92');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '26f3288ef1488b548c10');
