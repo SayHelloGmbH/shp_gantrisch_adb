a81ed554843120072666e8b2eb298f775995d692
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-03-27 14:39:42 +0200

    Offer map as iFrame

--- src/Blocks/OfferMap/assets/dist/scripts/editor.asset.php
+++ src/Blocks/OfferMap/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '714c0e1bc467854fb31a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1a78c8f0b3d5c6e55b65');
--- src/Blocks/OfferMap/render.php
+++ src/Blocks/OfferMap/render.php
@@ -2,7 +2,6 @@
 
 namespace SayHello\ShpGantrischAdb\Blocks\OfferTimeTermin;
 
-use SayHello\ShpGantrischAdb\Controller\API as APIController;
 use SayHello\ShpGantrischAdb\Package\Gutenberg as GutenbergPackage;
 
 shp_gantrisch_adb_get_instance()->Controller->Block->extend($block);
@@ -12,7 +11,7 @@ $gutenberg_package = new GutenbergPackage();
 if ($gutenberg_package->isContextEdit()) {
 ?>
 	<div class="<?php echo $block['shp']['class_names']; ?>">
-		<div class="c-message c-message--error">
+		<div class="c-message c-message--info">
 			<p><?php _ex('Placeholder for ADB map.', 'Editor preview message', 'shp_gantrisch_adb'); ?></p>
 		</div>
 	</div>
@@ -26,19 +25,10 @@ if (!$offer) {
 	return;
 }
 
-$api_controller = new APIController();
-$api = $api_controller->getApi();
-
-
-// Set selected park
-//$api->_set_selected_park($offer->park_id);
-
-// Load view
-// echo $api->_load_maps_api();
-//return;
-
 ?>
 
 <div class="<?php echo $block['shp']['class_names']; ?>">
-	<div class="<?php echo $block['shp']['classNameBase']; ?>__content"><?php $api->show_offers_map(); ?></div>
+	<div class="<?php echo $block['shp']['classNameBase']; ?>__content">
+		<iframe class="<?php echo $block['shp']['classNameBase']; ?>__iframe" src="https://angebote.paerke.ch/de/export/iframe/detail/0a54fe6d6aaa9a5956937700ef1201de31eabd49/<?php echo $offer->offer_id; ?>?map_only=true">
+	</div>
 </div>
