fa797dbe02d69e2574ea5e9b2d324eec423e7b6b
Mark Howells-Mead <mark@sayhello.ch>
2023-04-13 12:41:19 +0200

    Catch JS error

--- src/Blocks/OfferSameCategory/assets/src/scripts/view/index.js
+++ src/Blocks/OfferSameCategory/assets/src/scripts/view/index.js
@@ -1,5 +1,14 @@
 // jquery-match-height is loaded as one of the main scripts as it's used in several places
 
-jQuery.fn.matchHeight._throttle = 350;
-jQuery(`.wp-block-shp-adb-offer-same-category__entry-title`).matchHeight({});
-jQuery(`.wp-block-shp-adb-offer-same-category__entry-header`).matchHeight({});
+if (
+	typeof jQuery.fn !== 'undefined' &&
+	typeof jQuery.fn.matchHeight !== 'undefined'
+) {
+	jQuery.fn.matchHeight._throttle = 350;
+	jQuery(`.wp-block-shp-adb-offer-same-category__entry-title`).matchHeight(
+		{}
+	);
+	jQuery(`.wp-block-shp-adb-offer-same-category__entry-header`).matchHeight(
+		{}
+	);
+}
