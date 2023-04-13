173171cf726714bd97d539dfc91cc14a20db4d77
Mark Howells-Mead <mark@sayhello.ch>
2023-04-13 12:41:19 +0200

    Catch JS error

--- src/Blocks/OfferSameCategory/assets/dist/scripts/view.js
+++ src/Blocks/OfferSameCategory/assets/dist/scripts/view.js
@@ -1,1 +1,1 @@
-jQuery.fn.matchHeight._throttle=350,jQuery(".wp-block-shp-adb-offer-same-category__entry-title").matchHeight({}),jQuery(".wp-block-shp-adb-offer-same-category__entry-header").matchHeight({});
+void 0!==jQuery.fn&&void 0!==jQuery.fn.matchHeight&&(jQuery.fn.matchHeight._throttle=350,jQuery(".wp-block-shp-adb-offer-same-category__entry-title").matchHeight({}),jQuery(".wp-block-shp-adb-offer-same-category__entry-header").matchHeight({}));
