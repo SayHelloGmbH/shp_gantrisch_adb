1c5de96eb967fc026d51cb3e7467542fb162d037
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-04-24 18:20:26 +0200

    Add map iframe sizer

--- src/Blocks/OfferMap/render.php
+++ src/Blocks/OfferMap/render.php
@@ -29,6 +29,24 @@ if (!$offer) {
 
 <div class="<?php echo $block['shp']['class_names']; ?>">
 	<div class="<?php echo $block['shp']['classNameBase']; ?>__content">
-		<iframe class="<?php echo $block['shp']['classNameBase']; ?>__iframe" src="https://angebote.paerke.ch/de/export/iframe/detail/0a54fe6d6aaa9a5956937700ef1201de31eabd49/<?php echo $offer->offer_id; ?>?map_only=true"></iframe>
+		<iframe id="adb_iframe" class="<?php echo $block['shp']['classNameBase']; ?>__iframe" data-src="https://angebote.paerke.ch/de/export/iframe/detail/0a54fe6d6aaa9a5956937700ef1201de31eabd49/<?php echo $offer->offer_id; ?>?map_only=true"></iframe>
 	</div>
 </div>
+
+<script>
+	const iframe = document.getElementById('adb_iframe');
+	iframe.addEventListener('load', () => {
+		let script = document.createElement('script');
+		script.addEventListener('load', () => {
+			iFrameResize({}, '#adb_iframe');
+		});
+		document.head.appendChild(script);
+		script.setAttribute(
+			'src',
+			'https://angebote.paerke.ch/js/iframe-resizer/iframeResizer.min.js'
+		);
+
+	});
+	iframe.setAttribute('src', iframe.getAttribute('data-src'));
+	iframe.classList.add('is--loaded');
+</script>
