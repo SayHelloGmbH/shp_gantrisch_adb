9e845d017b6b3bd58febe5685b179ca3cdd33c47
Mark Howells-Mead <mark@sayhello.ch>
2023-04-26 10:56:47 +0200

    Remove filter

--- shp_gantrisch_adb.php
+++ shp_gantrisch_adb.php
@@ -4,7 +4,7 @@
  * Plugin URI:        https://bitbucket.org/sayhellogmbh/shp_gantrisch_adb/
  * Description:       Plugin for the output of offers from the Swiss Parks database.
  * Author:            Say Hello GmbH
- * Version:           1.7.4
+ * Version:           1.7.5
  * Author URI:        https://sayhello.ch/
  * Text Domain:       shp_gantrisch_adb
  * Domain Path:       /languages
