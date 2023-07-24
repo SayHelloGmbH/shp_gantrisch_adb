447de144147828024241ac9aba68e8b8e0c5953b
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-07-24 10:50:57 +0200

    Mod block description

--- src/Blocks/ListDefault/assets/dist/scripts/editor.asset.php
+++ src/Blocks/ListDefault/assets/dist/scripts/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks'), 'version' => '4b87453b1ad471c860d4');
+<?php return array('dependencies' => array('wp-blocks'), 'version' => 'cecdbc56f767c8baa5c7');
