2a3b4374470d4f8c725c5c4469ac2e0cc831c1b4
Mark Howells-Mead <mark@sayhello.ch>
2024-04-09 14:31:29 +0200

    Disable content ordering etc (temp)

--- src/Blocks/ListDefault/Block.php
+++ src/Blocks/ListDefault/Block.php
@@ -16,13 +16,13 @@ class Block
 		add_action('acf/init', [$this, 'registerFields']);
 
 		// All blocks. Block type check is in the method.
-		add_action('render_block', [$this, 'modifyHTML'], 10, 3);
+		//add_action('render_block', [$this, 'modifyHTML'], 10, 3);
 
 		// Only the main list
-		add_action('render_block_acf/shp-adb-list-default', [$this, 'sortEntries'], 20);
+		//add_action('render_block_acf/shp-adb-list-default', [$this, 'sortEntries'], 20);
 
 		// All blocks. Block type check is in the method.
-		add_action('render_block', [$this, 'contentOrder'], 30, 2);
+		//add_action('render_block', [$this, 'contentOrder'], 30, 2);
 	}
 
 	public function register()
